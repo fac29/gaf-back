b0025bb47258558b5f6d1bff1073ef42a2dc5720
thatGui <thatguifonseca@gmail.com>
2024-06-12 13:59:25 +0100

    trialTing

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-import express from 'express';
+import express, { Express, Application, Request, Response } from 'express';
 import sqlite3 from 'sqlite3';
 import dotenv from 'dotenv';
 import Products from './routes/product.routes';
+const cors = require('cors');
 
-const app = express();
+const app: Express = express();
 dotenv.config();
 const port = process.env.PORT || 3000;
+
+app.use(
+	'*',
+	cors({
+		origin: 'http://localhost:5173/',
+	}),
+);
+app.use(express.json());
 
 // Create a new SQLite database (or open an existing one)
 const db = new sqlite3.Database(':memory:');
