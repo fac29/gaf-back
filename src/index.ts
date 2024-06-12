914ddf18836a0a212148971b1d6a4b1ce208811e
Alexander <alex@alexvoiceover.com>
2024-06-12 22:37:24 +0100

    Added alternative origin to cors

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,8 @@ const port = process.env.PORT || 3000;
 
 app.use(
 	cors({
-		origin: 'http://localhost:5173',
+		//origin: 'http://localhost:5173',
+		origin: 'http://127.0.0.1:5173',
 	}),
 );
 app.use(express.json());
