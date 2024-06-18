af32495e0a758a17e99d80f0255b0cd639932b17
Alexander <alex@alexvoiceover.com>
2024-06-18 17:59:16 +0100

    Moved cookie secret to .env

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import sqlite3 from 'sqlite3';
 import dotenv from 'dotenv';
 import Products from './routes/product.routes';
 import { Users } from './routes/user.routes';
-import { Auth } from './routes/auth.routes'
+import { Auth } from './routes/auth.routes';
 import { Cart } from './routes/cart.routes';
 import { Reviews } from './routes/review.routes';
 import cors from 'cors';
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 
 const app: Express = express();
@@ -22,17 +22,16 @@ app.use(
 app.use(express.json());
 
 app.use(bodyParser.json());
-app.use(cookieParser('hlkjfasdfasdfasdfasddfasdfasdfas123321dlsadf'))
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // Create a new SQLite database (or open an existing one)
 const db = new sqlite3.Database(':memory:');
 
-Products(app)
-Users(app)
-Auth(app)
-Cart(app)
-Reviews(app)
-
+Products(app);
+Users(app);
+Auth(app);
+Cart(app);
+Reviews(app);
 
 app.listen(port, () => {
 	console.log(`Server is running at http://localhost:${port}`);
